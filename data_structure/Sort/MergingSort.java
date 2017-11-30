ebbf8fde0988667f143c12c033e6177f28b237b3
DefinitionBeyond <100203813@qq.com>
2017-11-30 11:17:38 +0800

    1130归并

--- data_structure/Sort/MergingSort.java
+++ data_structure/Sort/MergingSort.java
@@ -3,40 +3,56 @@ package data_structure.Sort;
 public class MergingSort {
     public static void main(String[] args) {
         int[] a = {6, 5, 3, 1, 8, 7, 2, 4};
-    }
-    public int[] MSorted(int[] a,int[] Sort,int start,int end){
-        int mid;
-        int Sort2[] = new int[a.length+1];
-        if(start == end){
-            Sort[start] = a[start];
-        }
-        else {
-            mid = (start+end)/2;
-            MSorted(a,Sort2,start,mid);
-            MSorted(a,Sort2,mid,end);
-            Merge(Sort2,Sort,start,mid,end);
-        }
-        return a;
+        MergingSort msort = new MergingSort();
+
+        msort.print(msort.MSort(a, 0, a.length - 1));
+
     }
 
-    private void Merge(int[] sort2, int[] sort, int start, int mid, int end) {
-        for(int i = mid+1,k=start;start<=mid&&i<=end;k++){
-            if(sort2[start]<sort2[i]){
-             sort[k] = sort2[start++];
-            }else {
-                sort[k] = sort2[i++];
+    public  int[] MSort(int[] nums, int low, int high) {
+        int mid = low + (high - low) / 2;
+        if (low < high) {
+            MSort(nums, low, mid);
+            MSort(nums, mid + 1, high);
+            Merge(nums, low, mid, high);
+        }
+        return nums;
+    }
+
+    private  void Merge(int[] nums, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;// 左指针
+        int j = mid + 1;// 右指针
+        int k = 0;
+
+        // 把较小的数先移到新数组中
+        while (i <= mid && j <= high) {
+            if (nums[i] < nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
             }
         }
-        if(start<mid){
-            for(int l =0 ; l<mid;l++){
-//                sort[k+l]
-            }
+
+        // 把左边剩余的数移入数组
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+
+        // 把右边边剩余的数移入数组
+        while (j <= high) {
+            temp[k++] = nums[j++];
+        }
+
+        // 把新数组中的数覆盖nums数组
+        for (int k2 = 0; k2 < temp.length; k2++) {
+            nums[k2 + low] = temp[k2];
         }
     }
 
-    public void print(int a[]){
-        for (int i = 0 ; i < a.length ;i++){
-            System.out.print(a[i]+"  ");
+    public void print(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "  ");
         }
     }
 }
