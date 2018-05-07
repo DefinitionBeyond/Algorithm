09d26bf0da5831584191cd22dcf82e4da57e2a48
DefinitionBeyond <100203813@qq.com>
2018-05-07 16:31:36 +0800

    二叉搜索树实现删除节点

--- data_structure/Tree/binarySearchTree/BinarySearchTree.java
+++ data_structure/Tree/binarySearchTree/BinarySearchTree.java
@@ -83,6 +83,96 @@ public class BinarySearchTree<T extends Comparable> {
         }
     }
 
+    /**
+     * @param key 节点的key值
+     * @return 是否成功删除
+     */
+    public boolean delete(int key) {
+        Node cur = root;
+        Node parent = root;
+        boolean isLeftChild = true;
+
+        /**
+         * 找到要删除的节点，并把当前结点定位到要删除的节点
+         */
+        while (cur.key != key) {
+            parent = cur;
+            if (key < cur.key) { //如果删除的元素在左子树
+                isLeftChild = true;
+                cur = cur.left;
+            } else { //如果删除的元素在右子树
+                isLeftChild = false;
+                cur = cur.right;
+            }
+            if (cur == null) {
+                return false;
+            }
+        }
+
+        /**
+         * 删除节点
+         */
+        if (cur.left == null && cur.right == null) {//当前结点是叶子节点的时候
+            if (cur == root) { //是根节点，且根节点没有叶子节点的时候
+                root = null; // 树为空树
+            } else if (isLeftChild) {// 当前结点是左孩子节点
+                parent.left = null; //父节点的左孩子设为空
+            } else {// 当前结点是右孩子节点
+                parent.right = null;//父节点的左孩子设为空
+            }
+        } else if (cur.right == null) {//当前结点没有右孩子，只有左孩子
+            if (cur == root) { //当前结点为根
+                root = cur.left;//左子树上移
+            } else if (isLeftChild) { // 当前结点是其父节点的左孩子
+                parent.left = cur.left;
+            } else { // 当前结点是其父节点的右孩子
+                parent.right = cur.left;
+            }
+        } else if (cur.left == null) {//当前结点没有左孩子，只有右孩子
+            if (cur == root) { //当前结点为根
+                root = cur.right;//左子树上移
+            } else if (isLeftChild) { // 当前结点是其父节点的左孩子
+                parent.left = cur.right;
+            } else { // 当前结点是其父节点的右孩子
+                parent.right = cur.right;
+            }
+        } else {//当前结点有左孩子，也有右孩子
+            Node<T> successor = getSuccessor(cur);
+            if (cur == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.left = successor;
+            } else {
+                parent.right = successor;
+            }
+            successor.left = cur.left;
+        }
+        return true;
+    }
+
+    /**
+     * 将要删除节点为根节点的树
+     * 进行旋转操作
+     *
+     * @param delNode 要删除的节点
+     * @return 旋好的子树
+     */
+    private Node<T> getSuccessor(Node delNode) {
+        Node<T> successorParent = delNode;
+        Node<T> successor = delNode;
+        Node<T> cur = delNode.right;
+        while (cur != null) {
+            successorParent = successor;
+            successor = cur;
+            cur = cur.left;
+        }
+        if (successor != delNode.right) {
+            successorParent.left = successor.right;
+            successor.right = delNode.right;
+        }
+        return successor;
+    }
+
 
     public void scanPreOrder(BinarySearchTree obj) {
         if (obj.isKV == true) {
--- data_structure/Tree/binarySearchTree/test.java
+++ data_structure/Tree/binarySearchTree/test.java
@@ -13,12 +13,22 @@ public class test {
     @Test
     public void go() {
         BinarySearchTree<String> tree = new BinarySearchTree<>(true);
-        tree.insert(5, "one");
-        tree.insert(3, "two");
-        tree.insert(2, "three");
-        tree.insert(7, "four");
-        tree.insert(6, "five");
-        tree.insert(4, "six");
+//        tree.insert(5, "one");
+//        tree.insert(3, "two");
+//        tree.insert(2, "three");
+//        tree.insert(7, "four");
+//        tree.insert(6, "five");
+//        tree.insert(4, "six");
+        tree.insert(9, " ");
+        tree.insert(4, " ");
+        tree.insert(2, " ");
+        tree.insert(18, " ");
+        tree.insert(14, " ");
+        tree.insert(24, " ");
+//        tree.insert(21," ");
+        tree.insert(27, " ");
+//        tree.insert(19," ");
+        tree.delete(18);
         tree.scanPreOrder(tree);
     }
 
